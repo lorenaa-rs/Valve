68c6917d113b61c1783a19090b058a870b06159b
lorenaa-rs <lorerios.2000@gmail.com>
2023-04-20 15:56:47 -0500

    Mensaje de confirmación

---
+++ api/src/main/java/com/valve/api/controller/PlayerController.java
@@ -0,0 +1,49 @@
+package com.valve.api.controller;
+
+import com.valve.api.entities.Player;
+import com.valve.api.services.PlayerService;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return playerService.getAllPlayers();
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayerById(@PathVariable Long id) {
+        return playerService.getPlayerById(id);
+    }
+
+    @PostMapping("/players")
+    public Player createPlayer(@RequestBody Player player) {
+        return playerService.createPlayer(player);
+    }
+
+    @PutMapping("/players/{id}")
+    public Player updatePlayer(@PathVariable Long id, @RequestBody Player player) {
+        return playerService.updatePlayer(id, player);
+    }
+
+    @DeleteMapping("/players/{id}")
+    public void deletePlayer(@PathVariable Long id) {
+        playerService.deletePlayer(id);
+    }
+}
